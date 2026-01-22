0ee245f5ac511baf7400cb982b3e0fa85e2eb5a8
Juan Felipe Sanchez Gonzalez <juan.sanchez@OCONTACT.local>
2026-01-22 15:20:46 -0500

    Actualización diseño 10

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const canvas = document.getElementById("stars");
+const ctx = canvas.getContext("2d");
+
+function resize() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+resize();
+window.addEventListener("resize", resize);
+
+let stars = [];
+const STAR_COUNT = 150;
+
+class Star {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.r = Math.random() * 1.5 + 0.5;
+    this.dx = (Math.random() - 0.5) * 0.3;
+    this.dy = (Math.random() - 0.5) * 0.3;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.fillStyle = "white";
+    ctx.fill();
+  }
+  update() {
+    this.x += this.dx;
+    this.y += this.dy;
+    if (this.x < 0 || this.x > canvas.width) this.x = Math.random() * canvas.width;
+    if (this.y < 0 || this.y > canvas.height) this.y = Math.random() * canvas.height;
+  }
+}
+
+function init() {
+  stars = [];
+  for (let i = 0; i < STAR_COUNT; i++) stars.push(new Star());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  stars.forEach(s => {
+    s.update();
+    s.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
